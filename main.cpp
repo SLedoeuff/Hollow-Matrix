299c3fbc2531f7552bf3986b4e9083cfecab2895
Faellyss <Faellyss@users.noreply.github.com>
2016-03-01 17:01:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
 #include <math.h>
-#include <string>
+#include <stdlib.h>
+#include <string.h>
 #include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#ifndef _test
+#define _test
 
 using namespace std;
 
-int main()
+
+class lcv
 {
+	private:
+		int li;
+		int co;
+		int val;
+		lcv* suiv;
+      		friend class matrice;
+
+	public:
+		lcv(int,int,int);
 		
-}
+		
+};
+
+class matrice
+{
+	private:
+		lcv *tete;
+
+	public:
+		matrice();
+		void ajout(int,int,int);
+		void affiche();
+		void addition(matrice);
+		void soustraction();
+		void multiplication();
+		//void carré();
+		void transposition();
+
+
+		void lecture();
+		void ecriture();
+		
+		
+};
+
+
+#endif
