e92e3464528ccb6e048dd830d4583cabc471dcb8
Faellyss <Faellyss@users.noreply.github.com>
2016-03-22 17:27:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,30 +13,44 @@ using namespace std;
 int main() {
 	cout<<endl;
 /*--------------test ajout--------------*/
-	/*matrice A ("A");
-	A.ajout(1,2,3,"ajj");
-	A.ajout(4,5,6,"ajj");
-	A.ajout(1,2,4,"ajj");
+	/*matrice A ("A",10,10);
+	A.ajout(1,2,3);
+	A.ajout(4,5,6);
+	A.ajout(1,2,4);
 	
 	A.affiche();*/
 
 /*-----------------test addition/soustraction mat---------*/
-	matrice A ("A");
-	matrice B ("B");
-	A.ajout(1,2,3,"ajj");
-	A.ajout(4,5,6,"ajj");
-	B.ajout(4,5,7,"ajj");
-	B.ajout(7,8,9,"ajj");
+	/*matrice A ("A",10,10);
+	matrice B ("B",10,10);
+	A.ajout(1,2,3);
+	A.ajout(4,5,6);
+	B.ajout(4,5,7);
+	B.ajout(7,8,9);
 	A.affiche();
 	B.affiche();
 	A.addition(B);
 	A.affiche();
-	matrice C ("C");
-	C.ajout(1,2,4,"ajj");
-	C.ajout(7,8,5,"ajj");
+	matrice C ("C",10,10);
+	C.ajout(1,2,4);
+	C.ajout(7,8,5);
 	C.affiche();
 	A.soustraction(C);
+	A.affiche();*/
+
+/*-----------------test mult mat---------*/
+	matrice A ("A",3,3);
+	matrice B ("B",3,3);
+	A.ajout(1,1,2);
+	A.ajout(2,1,2);
+	B.ajout(1,1,2);
+	B.ajout(1,2,2);
 	A.affiche();
+	B.affiche();
+	matrice C=A.multiplication(B);
+	C.affiche();
+	C=C.carre();
+	C.affiche();
+
   return 0;
 }	
-
