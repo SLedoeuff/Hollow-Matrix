cc5b44bc14fa06c9c31dd5ed8c185f4e6a8d9058
Faellyss <Faellyss@users.noreply.github.com>
2016-03-22 17:28:55 +0100

    Update libb.cpp

--- libb.cpp
+++ libb.cpp
@@ -20,20 +20,23 @@ lcv::lcv(int x,int y,int z)
 }
 
 
-matrice::matrice(string nom)
+matrice::matrice(string nom,int x,int y)
 {
+	hauteur=y;
+	largeur=x;
 	name=nom;
 	tete=NULL;
 }
 
 
-void matrice::ajout(int x,int y,int z,string action)
+void matrice::action(int x,int y,int z,string act)
 {
 	if(tete==NULL)
 	{
 		lcv *B=new lcv(x,y,z);
 		tete=B;
-		if(action=="ajj")cout<<"Ajout de la valeur "<<z<<" a la position ("<<x<<"."<<y<<") dans la matrice "<<this->name<<endl;
+		if(act=="ajj")cout<<"Ajout de la valeur "<<z<<" a la position ("<<x<<"."<<y<<") dans la matrice "<<this->name<<endl;
+		
 		return;
 	}
 	lcv* temp=tete;
@@ -41,8 +44,8 @@ void matrice::ajout(int x,int y,int z,string action)
 	{
 		if(temp->li==x && temp->co==y)
 		{
-			if(action=="ajj")cerr<<"Erreur il y a deja la valeur "<<temp->val<<" a la position ("<<temp->li<<"."<<temp->co<<") , impossible d ajouter la valeur dans la matrice "<<this->name<<z<<endl;
-			else if(action=="add")
+			if(act=="ajj")cerr<<"Erreur il y a deja la valeur "<<temp->val<<" a la position ("<<temp->li<<"."<<temp->co<<") , impossible d ajouter la valeur dans la matrice "<<this->name<<z<<endl;
+			else if(act=="add")
 			{
 				temp->val=temp->val+z;
 			}
@@ -52,8 +55,8 @@ void matrice::ajout(int x,int y,int z,string action)
 	}
 	if(temp->li==x && temp->co==y)
 	{
-		if(action=="ajj")cerr<<"Erreur il y a deja la valeur "<<temp->val<<" a la position ("<<temp->li<<"."<<temp->co<<") , impossible d ajouter la valeur dans la matrice "<<this->name<<z<<endl;
-		else if(action=="add")
+		if(act=="ajj")cerr<<"Erreur il y a deja la valeur "<<temp->val<<" a la position ("<<temp->li<<"."<<temp->co<<") , impossible d ajouter la valeur dans la matrice "<<this->name<<z<<endl;
+		else if(act=="add")
 		{
 			temp->val=temp->val+z;
 		}
@@ -61,7 +64,7 @@ void matrice::ajout(int x,int y,int z,string action)
 	}
 	lcv* B=new lcv (x,y,z);
 	temp->suiv=B;
-	if(action=="ajj")cout<<"Ajout de la valeur "<<z<<" a la position ("<<x<<"."<<y<<") dans la matrice "<<this->name<<endl;
+	if(act=="ajj")cout<<"Ajout de la valeur "<<z<<" a la position ("<<x<<"."<<y<<") dans la matrice "<<this->name<<endl;
 	
 }
 
@@ -72,44 +75,127 @@ void matrice::affiche()
 	lcv* temp=tete;
 	while(temp->suiv!=NULL)
 	{
-		cout<<setw(10)<<right<<temp->li<<setw(10)<<right<<temp->co<<setw(10)<<right<<temp->val<<endl;
-		temp=temp->suiv;
+		if(temp->val!=0)
+		{
+			cout<<setw(10)<<right<<temp->li<<setw(10)<<right<<temp->co<<setw(10)<<right<<temp->val<<endl;
+			temp=temp->suiv;
+		}
 	}
 	cout<<setw(10)<<right<<temp->li<<setw(10)<<right<<temp->co<<setw(10)<<right<<temp->val<<endl;
 	cout<<endl;
 
 }
+void matrice::ajout(int x,int y,int z)
+{
+	if(z==0)
+	{
+		cout<<"ajout de 0 inutile dans la matrice"<<endl;
+		return;
+	}
+	if(x<=largeur && y<=hauteur)
+	{
+		action(x,y,z,"ajj");
+	}
+	else
+	{
+		cout<<"Erreur coordonnées hors matrice ("<<x<<","<<y<<")"<<endl;
+	}
+	
+}
+
 
 void matrice::addition(matrice B)
 {
-	lcv* temp=B.tete;
-	while(temp->suiv!=NULL)
+	if(hauteur==B.hauteur && largeur ==B.largeur)
 	{
-		ajout(temp->li,temp->co,temp->val,"add");
-		temp=temp->suiv;
+		lcv* temp=B.tete;
+		while(temp->suiv!=NULL)
+		{
+			action(temp->li,temp->co,temp->val,"add");
+			temp=temp->suiv;
+		}
+		action(temp->li,temp->co,temp->val,"add");
 	}
-	ajout(temp->li,temp->co,temp->val,"add");
+	else
+	{
+		cout<<"Erreur les deux matrice doivent avoir des tailles identique"<<endl;
+	}
 }
 
 
 void matrice::soustraction(matrice B)
-{
-	lcv* temp=B.tete;
-	while(temp->suiv!=NULL)
+{	
+	if(hauteur==B.hauteur && largeur ==B.largeur)
 	{
-		ajout(temp->li,temp->co,-(temp->val),"add");
-		temp=temp->suiv;
+		lcv* temp=B.tete;
+		while(temp->suiv!=NULL)
+		{
+			action(temp->li,temp->co,-(temp->val),"add");
+			temp=temp->suiv;
+		}
+		action(temp->li,temp->co,-(temp->val),"add");
 	}
-	ajout(temp->li,temp->co,-(temp->val),"add");
+	else
+	{
+		cout<<"Erreur les deux matrice doivent avoir des tailles identique"<<endl;
+	}
 }
 
 
-void matrice::multiplication(matrice B){}
-/*void matrice::carre()
+
+matrice matrice::multiplication(matrice B)
 {
-	this->multiplication(this);
+	if(largeur==B.hauteur)
+	{
+		matrice res ("res",hauteur,B.largeur);
+		lcv* temp=tete;
+		while(temp->suiv!=NULL)
+		{
+			lcv* temp2=B.tete;
+			while(temp2->suiv!=NULL)
+			{
+				if(temp2->li==temp->co)
+				{
+					res.action(temp->li,temp2->co,(temp->val*temp2->val),"add");
+				}
+				temp2=temp2->suiv;
+			}
+			if(temp2->li==temp->co)
+			{
+				res.action(temp->li,temp2->co,(temp->val*temp2->val),"add");
+			}
+			temp=temp->suiv;
+		}
+
+		lcv* temp2=B.tete;
+		while(temp2->suiv!=NULL)
+		{
+			if(temp2->li==temp->co)
+			{
+				res.action(temp->li,temp2->co,(temp->val*temp2->val),"add");
+			}
+			temp2=temp2->suiv;
+		}
+		if(temp2->li==temp->co)
+		{
+			res.action(temp->li,temp2->co,(temp->val*temp2->val),"add");
+		}
+		return res;
+	}
+	else
+	{
+		cout<<"Erreur la hauteur de la premiere matrice doit etre egal a la largeur de la deuxieme"<<endl;
+	}
+
+}
+
+
+
+matrice matrice::carre()
+{
+	return multiplication(*this);
 	
-}*/
+}
 void matrice::transposition()
 {
     int a;
